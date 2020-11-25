0076f45f3374e4fb2527f05fe22f00e46f659ffb
yangyang <1434753182@qq.com>
2020-11-25 19:33:34 +0800

    首页轮播图结构

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
-import login from "../../store/action/login";
+// import login from "../../store/action/login";
+import Tab from "../../common/component/tab"
+import "../../common/css/index.css"
+
+let imgData = [
+  require("../../common/images/banner1.jpg").default,
+  require("../../common/images/banner2.jpg").default,
+  require("../../common/images/banner3.jpg").default,
+  require("../../common/images/banner4.jpg").default,
+]
 
 function Index(props) {
   // console.log(props);
   return (
     <div>
-      {[...".".repeat(20)].map((item,index) => {
+{/*       {[...".".repeat(20)].map((item,index) => {
         return <div key={index}>我是首页</div>;
       })}
       <button
@@ -15,7 +24,13 @@ function Index(props) {
         }}
       >
         登录
-      </button>
+      </button> */}
+      <Tab
+      data={imgData}
+      render={(data)=>{
+        return <img src={data}></img>
+      }}
+      ></Tab>
     </div>
   );
 }
