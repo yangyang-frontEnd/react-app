37be1d19fcb7a6255631e4e416818813bb0e3761
liwan <2715373792@qq.com>
2020-11-27 19:26:22 +0800

    dev-lw v1.1

--- src/view/lecturer/index.js
+++ src/view/lecturer/index.js
@@ -106,6 +106,22 @@ function Lecturer(props) {
           </p>
         </div>
     </div>
+    <footer id="footer">
+        <div class="link clearfix">
+            <a href="#">hr_recruit@miaov.com</a>
+            <a href="#">010-57269690</a>
+        </div>
+        <nav class="foot_nav clearfix">
+            <a href="#">网站首页</a>
+            <a href="#">课程安排</a>
+            <a href="#">学员作品</a>
+            <a href="#">视频教程</a>
+            <a href="#">关于我们</a>
+            <a href="#">在线留言</a>
+            <a href="#">常见问题</a>
+        </nav>
+        <p class="copyright">京ICP备08102442号-1 2007-2016 MIAOOV.COM 版权所有</p>
+    </footer>
   </div>;
 }
 
